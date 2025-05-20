7c46674bec258401da9ff05c47930c745017b839
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-05-20 16:37:08 +0530

    Add Navigation for Login and Signup Screen

---
+++ app/src/main/java/com/ojasx/eduplay/Navigation/AppNavigation.kt
@@ -0,0 +1,33 @@
+package com.ojasx.eduplay.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ojasx.eduplay.LoginOrSignUpPage.LoginPage.LoginPage
+import com.ojasx.eduplay.LoginOrSignUpPage.SignUpPgae.SignUpPage
+import com.ojasx.eduplay.MainScreen.MainScreen
+import kotlinx.coroutines.flow.combine
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "MainScreen"
+    ) {
+        composable("MainScreen") {
+            MainScreen(navController)
+        }
+        composable("LoginScreen") {
+            LoginPage(navController)
+        }
+        composable("SignUpScreen") {
+            SignUpPage(navController)
+        }
+
+
+    }
+}
