1c2eb67ec6d684d8cbc152b4a1e7b85c9768ff5c
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-05-20 13:26:25 +0530

    Add Login Page without FireBase Authentication

---
+++ app/src/main/java/com/ojasx/eduplay/LoginOrSignUpPage/LoginPage/LoginPic.kt
@@ -0,0 +1,59 @@
+package com.ojasx.eduplay.LoginOrSignUpPage.LoginPage
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ojasx.eduplay.R
+
+@Composable
+fun LoginPic() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.login),
+            contentDescription = "",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(100.dp)
+        )
+    }
+}
