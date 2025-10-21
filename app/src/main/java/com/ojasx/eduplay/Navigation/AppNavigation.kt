61958dd4bee4b17835e98886710923747bf90501
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-10-21 11:54:29 +0530

    Add Navigation

---
+++ app/src/main/java/com/ojasx/eduplay/Navigation/AppNavigation.kt
@@ -0,0 +1,43 @@
+package com.ojasx.eduplay.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ojasx.eduplay.API.PlaylistScreen
+import com.ojasx.eduplay.API.PlaylistViewModel
+import com.ojasx.eduplay.HomePage
+import com.ojasx.eduplay.LoginOrSignUpPage.LoginPage.LoginPage
+import com.ojasx.eduplay.LoginOrSignUpPage.SignUpPgae.SignUpPage
+import com.ojasx.eduplay.MainScreen.MainScreen
+import kotlinx.coroutines.flow.combine
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val viewModel : PlaylistViewModel = viewModel()
+    NavHost(
+        navController = navController,
+        startDestination = "MainScreen"
+    ) {
+        composable("MainScreen") {
+            MainScreen(navController)
+        }
+        composable("LoginScreen") {
+            LoginPage(navController)
+        }
+        composable("SignUpScreen") {
+            SignUpPage(navController)
+        }
+        composable("Home") {
+            HomePage(navController)
+        }
+
+        composable("PlaylistScreen"){
+            PlaylistScreen(viewModel)
+        }
+    }
+}
