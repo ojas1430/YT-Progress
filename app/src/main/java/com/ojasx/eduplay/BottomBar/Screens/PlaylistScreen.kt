ec1c402510ee80f4ae30b9bdc4834035d984a92b
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-10-21 11:50:51 +0530

    Add Screens

---
+++ app/src/main/java/com/ojasx/eduplay/BottomBar/Screens/PlaylistScreen.kt
@@ -0,0 +1,9 @@
+package com.ojasx.eduplay.BottomBar.Screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PlaylistScreen(modifier: Modifier = Modifier) {
+    
+}
