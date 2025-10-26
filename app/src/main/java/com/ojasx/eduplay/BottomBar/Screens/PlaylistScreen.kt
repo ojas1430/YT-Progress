df3e18c9be8272ae4e4c23ee3ccc79f1aead6e01
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-10-26 13:12:37 +0530

    Update PlaylistScreen.kt (It displays link videos now)

--- app/src/main/java/com/ojasx/eduplay/BottomBar/Screens/PlaylistScreen.kt
+++ app/src/main/java/com/ojasx/eduplay/BottomBar/Screens/PlaylistScreen.kt
@@ -1,9 +1,17 @@
 package com.ojasx.eduplay.BottomBar.Screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.ojasx.eduplay.API.LinkPlaylistScreen
+import com.ojasx.eduplay.API.PlaylistViewModel
 
 @Composable
-fun PlaylistScreen(modifier: Modifier = Modifier) {
-    
+fun PlaylistScreen(
+    playlistviewModel: PlaylistViewModel = viewModel(),
+    navController: NavController
+) {
+    LinkPlaylistScreen(playlistviewModel)
 }
