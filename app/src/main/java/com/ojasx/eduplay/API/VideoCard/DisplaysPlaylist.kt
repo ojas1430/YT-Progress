e6b27f763e163bf058e0e868d8308f03c19e84cd
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-10-27 21:28:48 +0530

    add DisplaysPlaylist.kt

---
+++ app/src/main/java/com/ojasx/eduplay/API/VideoCard/DisplaysPlaylist.kt
@@ -0,0 +1,49 @@
+package com.ojasx.eduplay.API.VideoCard
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.ojasx.eduplay.API.PlaylistItem
+import com.ojasx.eduplay.API.PlaylistViewModel
+
+@Composable
+fun LinkPlaylistScreen(viewModel: PlaylistViewModel = viewModel()) {
+
+        val playlistItems by remember { derivedStateOf { viewModel.playlistItems.value } }
+
+        // Sort pinned videos on top
+        val sortedList = playlistItems.sortedByDescending { it.isPinned }
+
+    LazyColumn {
+        items(sortedList) { item ->
+
+            PlaylistItemCard(
+                item = item,
+                onCheckedChange = { checked ->
+                    item.isCompleted = checked
+                },
+                onNotesClick = {
+                    // Open notes dialog / bottom sheet here
+                    println("Notes clicked for ${item.snippet.title}")
+                },
+                onRevisionClick = {
+                    item.needsRevision = !item.needsRevision
+                },
+                isPinned = { pinned -> item.isPinned = pinned  }
+            )
+
+        }
+    }
+}
