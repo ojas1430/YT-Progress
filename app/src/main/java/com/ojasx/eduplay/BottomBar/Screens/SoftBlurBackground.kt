fa800297f098058c483b1798b337706e28254c05
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-10-29 00:07:02 +0530

    Add SoftBlurBackground.kt

---
+++ app/src/main/java/com/ojasx/eduplay/BottomBar/Screens/SoftBlurBackground.kt
@@ -0,0 +1,35 @@
+package com.ojasx.eduplay.BottomBar.Screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SoftBlurBackground() {
+
+    val PurpleGradient = Color(0xFFA770EF)
+    val PinkGradient = Color(0xFFCF8BF3)
+    val PeachGradient = Color(0xFFFDB99B)
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        PurpleGradient,
+                        PinkGradient,
+                        PeachGradient
+                    )
+                )
+            )
+            .blur(60.dp)
+    )
+}
