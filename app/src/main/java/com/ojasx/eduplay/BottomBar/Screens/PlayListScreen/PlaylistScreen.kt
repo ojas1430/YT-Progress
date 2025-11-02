0ec63c47329d1328fa75126169e1579506d8ac14
OJAS-CHOUDHARY <ojas1430@gmail.com>
2025-11-02 10:49:20 +0530

    Update PlaylistScreen.kt

--- app/src/main/java/com/ojasx/eduplay/BottomBar/Screens/PlayListScreen/PlaylistScreen.kt
+++ app/src/main/java/com/ojasx/eduplay/BottomBar/Screens/PlayListScreen/PlaylistScreen.kt
@@ -1,38 +1,38 @@
 package com.ojasx.eduplay.BottomBar.Screens.PlayListScreen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.ojasx.eduplay.API.VideoCard.LinkPlaylistScreen
 import com.ojasx.eduplay.API.PlaylistViewModel
-import com.ojasx.eduplay.R
-import com.ojasx.eduplay.StatusBar
+import com.ojasx.eduplay.API.VideoCard.LinkPlaylistScreen
+import com.ojasx.eduplay.BottomBar.Screens.SoftBlurBackground
 
 @Composable
 fun PlaylistScreen(
     playlistviewModel: PlaylistViewModel = viewModel(),
     navController: NavController
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-    ) {
-        MyPlaylistBanner()
 
-        //All playlist videos are displayed
-        LinkPlaylistScreen(playlistviewModel)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        SoftBlurBackground()
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+        ) {
+            MyPlaylistBanner()
+
+            // 🎥 All playlist videos
+            LinkPlaylistScreen(playlistviewModel)
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+        }
     }
 }
